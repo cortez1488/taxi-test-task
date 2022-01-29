65c390aeb0bee762ca2498a30c6d26261f2f8c99
cortez1488 <mlm.ui0@mail.ru>
2022-01-29 18:35:07 +0500

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"taxiTestTask/internal/json_to_struct"
-	reqToAPI "taxiTestTask/internal/reqToAPI/JSON"
-	"taxiTestTask/models"
-)
+import "taxiTestTask/pkg/app"
 
 func main() {
-
-	json, err := reqToAPI.GetJSONFromAPIRequest()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	var input []models.TaxiRawData
-	err = json_to_struct.ParseBodyJson(json, &input)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println(input)
+	app.Run()
 }
---
+++ pkg/app/app.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"taxiTestTask/internal/json_to_struct"
+	reqToAPI "taxiTestTask/internal/reqToAPI/JSON"
+	"taxiTestTask/models"
+)
+
+func Run() {
+	json, err := reqToAPI.GetJSONFromAPIRequest()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var input []models.TaxiRawData
+	err = json_to_struct.ParseBodyJson(json, &input)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(input)
+}
